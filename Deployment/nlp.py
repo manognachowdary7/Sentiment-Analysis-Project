c5dd4d98980df78b690cf6ec0a05c9b29ffd55dc
V.MANOGNA <65440296+manognachowdary7@users.noreply.github.com>
2024-02-05 15:33:47 +0530

    Update nlp.py

--- Deployment/nlp.py
+++ Deployment/nlp.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pickle
 from sklearn.feature_extraction.text import CountVectorizer
-from PIL import Image
 
 # Load the Decision Tree model
 model_path = 'best_classifier_decision_tree.pkl'
@@ -44,23 +43,6 @@ if st.button('Predict'):
 
             if prediction[0] in [0, 1, 2]:  # Check if the prediction is within the expected range
                 st.write('Prediction:', sentiment_labels[prediction[0]])
-
-                # Show a local image based on sentiment
-                if prediction[0] == 0:  # Negative
-                    image_path = r"C:\Users\ds_sa\Desktop\Project_P340\Gif\0.jpg"
-                    caption = "Sad___Negative___😥"
-                elif prediction[0] == 1:  # Neutral
-                    image_path = r"C:\Users\ds_sa\Desktop\Project_P340\Gif\1.jpg"
-                    caption = "Silent___Neutral___😐"
-                elif prediction[0] == 2:  # Positive
-                    image_path = r"C:\Users\ds_sa\Desktop\Project_P340\Gif\2.jpg"
-                    caption = "Always___Positive___☺️"
-
-            # Open the local image using Pillow (PIL)
-            image = Image.open(image_path)
-
-            # Display the image
-            st.image(image, caption=f"Your Sentiment Image: {caption}", use_column_width=True)
-
+                
         except Exception as e:
             st.error(f"An error occurred: {e}")
